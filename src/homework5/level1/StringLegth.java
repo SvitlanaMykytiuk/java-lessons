bee6322a79d55231a90f889a24bbebe7b623832e
SvitlanaMykytiuk <svetlana.mikityuk@gmail.com>
2022-10-06 19:42:53 +0300

    HomeWork5

--- src/homework5/level1/StringLegth.java
+++ src/homework5/level1/StringLegth.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class StringLegth {
     boolean isStringMoreThan10 (String string){
+
         return string.length() > 10;
     }
 
     public static void main(String[] args) {
         StringLegth stringLength = new StringLegth();
         Scanner scaner = new Scanner(System.in);
-        String string = scan.nextLine();
+        String string = scaner.nextLine();
         System.out.println(stringLength.isStringMoreThan10(string));
     }
 }
